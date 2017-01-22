574b4dfdf06f46fa948a634090c2dadfad480d2b
Ztreis <neilana@yandex.ru>
2017-01-22 19:17:45 +0000

    Initial commit

---
+++ updateFromGit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 1. i3 configuration
+cp i3_config/config ~/.config/i3/
+
+cp i3_config/i3blocks/i3blocks.conf /usr/lib/i3blocks/
+cp i3_config/i3blocks/myBlocks/* /usr/lib/i3blocks/myBlocks/
+
+# 2. Startup configs
+cp startup_config/.xinitrc ~/
+
+# 3. Color schemes (themes)
+# xterm colors
+cp appsColors_config/xterm/.Xresources ~/
+xrdb -load ~/.Xresources
+
+# sublime text 3 theme
+cp appsColors_config/sublime3/MyTheme.tmTheme ~/.config/sublime-text-3/Packages/Colorsublime-Themes/
+
+# ranger theme
+cp appsColors_config/ranger/MyTheme.py ~/.config/ranger/colorschemes/
+
+# cmus theme (nned to "colorscheme Mytheme2" in cmus to refresh theme (even if the theme is the same))
+cp appsColors_config/cmus/MyTheme2.theme /usr/share/cmus/
---
+++ updateToGit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 1. i3 configuration
+cp ~/.config/i3/config i3_config/
+
+cp /usr/lib/i3blocks/i3blocks.conf i3_config/i3blocks/
+cp /usr/lib/i3blocks/myBlocks/* i3_config/i3blocks/myBlocks
+
+# 2. Startup configs
+cp ~/.xinitrc startup_config/
+
+# 3. Color schemes (themes)
+# xterm colors
+cp ~/.Xresources appsColors_config/xterm/
+
+# sublime text 3 theme
+cp ~/.config/sublime-text-3/Packages/Colorsublime-Themes/MyTheme.tmTheme appsColors_config/sublime3
+
+# ranger theme
+cp ~/.config/ranger/colorschemes/MyTheme.py appsColors_config/ranger/
+
+# cmus theme
+cp /usr/share/cmus/MyTheme2.theme appsColors_config/cmus/
