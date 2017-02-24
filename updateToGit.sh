c3a7c6b988950f691551e759e623952f29182bdb
Alina Voropaeva <ztreis@yandex.ru>
2017-02-24 22:12:47 +0300

    Update updateToGit.sh

--- updateToGit.sh
+++ updateToGit.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # 1. i3 configuration
-cp ~/.config/i3/config i3_config/
+cp ~/.config/i3/* i3_config/
 
 cp /usr/lib/i3blocks/i3blocks.conf i3_config/i3blocks/
 cp /usr/lib/i3blocks/myBlocks/* i3_config/i3blocks/myBlocks
 
 # 2. Startup configs
 cp ~/.xinitrc startup_config/
+cp ~/.bashrc startup_config/
+cp ~/.vimrc startup_config/
+cp ~/.bash_profile startup_config/
 
 # 3. Color schemes (themes)
 # xterm colors
@@ -21,3 +24,6 @@ cp ~/.config/ranger/colorschemes/MyTheme.py appsColors_config/ranger/
 
 # cmus theme
 cp /usr/share/cmus/MyTheme2.theme appsColors_config/cmus/
+
+#4 keyboard
+#
